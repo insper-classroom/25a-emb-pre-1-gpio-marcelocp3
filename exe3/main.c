cb9cc62671f261afaaa00d879e581fdc3459294e
marcelocp3 <marcelocp3@al.insper.edu.br>
2025-02-12 20:05:12 -0300

    Ex 3

--- exe3/main.c
+++ exe3/main.c
@@ -2,9 +2,51 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
+const int BTN_PIN_VERM = 28;
+const int BTN_PIN_VERDE = 26;
+const int LED_PIN_VERM = 4;
+const int LED_PIN_VERDE = 6;
+
 int main() {
   stdio_init_all();
+  gpio_init(LED_PIN_VERM);
+  gpio_init(LED_PIN_VERDE);
+  gpio_set_dir(LED_PIN_VERM, GPIO_OUT);
+  gpio_set_dir(LED_PIN_VERDE,GPIO_OUT);
+
+  gpio_init(BTN_PIN_VERDE);
+  gpio_init(BTN_PIN_VERM);
+  gpio_set_dir(BTN_PIN_VERDE, GPIO_IN);
+  gpio_set_dir(BTN_PIN_VERM, GPIO_IN);  
+  gpio_pull_up(BTN_PIN_VERDE);
+  gpio_pull_up(BTN_PIN_VERM);
+
+  int i = 0;
+  int j = 0;
 
   while (true) {
+    if (!gpio_get(BTN_PIN_VERDE)) {
+      if (i == 0){
+        i = 1;
+        gpio_put(LED_PIN_VERDE,i);
+      } else{
+        i = 0;
+        gpio_put(LED_PIN_VERDE,i);
+      }
+      while (!gpio_get(BTN_PIN_VERDE)) {
+    };
   }
+
+  if (!gpio_get(BTN_PIN_VERM)){
+    if (j == 0){
+      j = 1;
+      gpio_put(LED_PIN_VERM,j);
+    } else{
+      j = 0;
+      gpio_put(LED_PIN_VERM,j);
+    }
+    while (!gpio_get(BTN_PIN_VERM)){
+    };
+  }
+}
 }
